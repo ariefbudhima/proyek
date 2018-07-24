ba0b22bb855dae4e87747944f9f46b99bcd51566
Rama Aditya Maulana <ramaadtym@gmail.com>
2018-07-24 15:13:23 +0700

    Update urin sama serologi

--- application/views/laman/comp/menulaborat.php
+++ application/views/laman/comp/menulaborat.php
@@ -15,9 +15,9 @@
         <li>
             <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="fa fa-plus-square"></i><span class="sidebar-mini-hide">Manajemen Laborat</span></a>
             <ul>
-                <li>
+                <!-- <li>
                     <a href="<?php echo base_url();?>inputLaborat/inputlaborathema">Lihat Data Laborat</a>
-                </li>
+                </li> -->
                 <li>
                     <a class="nav-submenu" data-toggle="nav-submenu" href="#">Manajemen Hematologi</a>
                     <ul>
